cdd79271a94f2a3a0ea8ccf4a6d8f2cdd520344b
zehan khan <zehan9211@gmail.com>
2024-05-28 13:45:36 +0530

    feat: add json server and custom data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("database.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use((req, res, next) => {
+    setTimeout(next, 500);
+});
+
+server.use(router);
 
 const app = express();
+app.use(server);
 
 app.get("/", (req, res) => {
     res.json({ message: "api is working" });
